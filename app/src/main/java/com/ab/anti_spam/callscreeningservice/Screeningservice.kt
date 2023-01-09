92ab6d675e99c6f26e5fafd7880f3b100e2e7610
Adrian Bernacki <adrianbernacki2021@gmail.com>
2023-01-09 12:29:48 +0000

    Permissions - Receive SMS permissions. - Read phone state permissions. - Call phone permissions. - Default Caller ID / Spam caller app permissions. - Call Screening service added for future.

---
+++ app/src/main/java/com/ab/anti_spam/callscreeningservice/Screeningservice.kt
@@ -0,0 +1,39 @@
+package com.ab.anti_spam.callscreeningservice
+
+import android.telecom.Call
+import android.telecom.CallScreeningService
+
+class Screeningservice: CallScreeningService() {
+    override fun onScreenCall(callDetails: Call.Details) {
+        val phoneNumber = getPhoneNumber(callDetails)
+        var response = CallResponse.Builder()
+        response = handlePhoneCall(response, phoneNumber)
+
+        respondToCall(callDetails, response.build())
+    }
+
+    private fun handlePhoneCall(
+        response: CallResponse.Builder,
+        phoneNumber: String
+    ): CallResponse.Builder {
+
+        val FORBIDDEN_PHONE_CALL_NUMBER = "<<PHONE NUMBER>>"
+        if (FORBIDDEN_PHONE_CALL_NUMBER in phoneNumber) {
+            response.apply {
+                setRejectCall(true)
+                setDisallowCall(true)
+                setSkipCallLog(true)
+                //
+                println((String.format("Rejected call from %s", phoneNumber)))
+            }
+        } else {
+            println(String.format("Incoming call from %s", phoneNumber))
+        }
+        return response
+    }
+
+    private fun getPhoneNumber(callDetails: Call.Details): String {
+        return callDetails.handle.toString().replace("tel:","")
+        //.removeTelPrefix().parseCountryCode()
+    }
+}
--- app/src/main/java/com/ab/anti_spam/ui/auth/Login.kt
+++ app/src/main/java/com/ab/anti_spam/ui/auth/Login.kt
@@ -1,18 +1,24 @@
 package com.ab.anti_spam.ui.auth
 
+import android.Manifest
+import android.app.Activity
+import android.app.role.RoleManager
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.ab.anti_spam.databinding.LoginBinding
-import androidx.lifecycle.Observer
+import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 
 //
 
-class Login: AppCompatActivity() {
-
+class Login: AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+    val perms = arrayOf(Manifest.permission.RECEIVE_SMS,Manifest.permission.READ_PHONE_STATE,Manifest.permission.CALL_PHONE)
     private lateinit var loginViewModel : LoginViewModel
     private lateinit var loginBinding: LoginBinding
     private lateinit var loggedInViewModel : LoggedInViewModel
@@ -41,11 +47,12 @@ class Login: AppCompatActivity() {
 
     private fun loginListener(){
         loginBinding.welcomeButton.setOnClickListener{
-          //  if(hasLocationPermission()) {
-                loginViewModel.login()
-          //  }else{
-             //   requestLocationPermission()
-           // }
+            if(hasPermissions() ) {
+                //If Default dialer is set, it ignores intent and responds with ACTIVITY OK, then continues.
+                requestDefaultCallerIDPermissions()
+            }else{
+                requestPermissions()
+            }
         }
     }
     override fun onStop() {
@@ -60,4 +67,57 @@ class Login: AppCompatActivity() {
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(aClass: Class<T>):T = f() as T
         }
+
+
+    private fun hasPermissions() = EasyPermissions.hasPermissions(applicationContext, *perms)
+    private fun requestPermissions(){
+
+    ActivityCompat.requestPermissions(this,perms,1)
+    requestDefaultCallerIDPermissions()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        //Give the user an option to choose permissions
+
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            AppSettingsDialog.Builder(this).build().show()
+
+        }else{
+            requestPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Toast.makeText(applicationContext,"Permissions Granted!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun requestDefaultCallerIDPermissions(){
+        startCallScreeningPermissionScreen(100)
+    }
+
+    fun Activity.startCallScreeningPermissionScreen(requestId: Int) {
+        val roleManager = this.getSystemService(AppCompatActivity.ROLE_SERVICE) as RoleManager
+        val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING)
+        //this.startActivityForResult(intent, requestId)
+        resultLauncher.launch(intent)
+
+    }
+
+    //Handles default dialer app response result
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+
+            loginViewModel.login()
+        }else{
+            requestDefaultCallerIDPermissions()
+        }
+    }
+
+
 }
