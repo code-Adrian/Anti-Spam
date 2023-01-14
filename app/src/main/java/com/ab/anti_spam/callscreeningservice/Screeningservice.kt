3bfbfd5bd8680100dd0a659642f3feb3a29fe880
Adrian Bernacki <adrianbernacki2021@gmail.com>
2023-01-14 10:48:36 +0000

    Call Screening Service - Call screening service now blocks by country, number or regex added from the recycler view, retrieved from localstorage.

--- app/src/main/java/com/ab/anti_spam/callscreeningservice/Screeningservice.kt
+++ app/src/main/java/com/ab/anti_spam/callscreeningservice/Screeningservice.kt
@@ -2,8 +2,11 @@ package com.ab.anti_spam.callscreeningservice
 
 import android.telecom.Call
 import android.telecom.CallScreeningService
+import android.telecom.CallScreeningService.CallResponse.Builder
+import com.ab.anti_spam.localstorage.CallBlacklistStorage
 
 class Screeningservice: CallScreeningService() {
+
     override fun onScreenCall(callDetails: Call.Details) {
         val phoneNumber = getPhoneNumber(callDetails)
         var response = CallResponse.Builder()
@@ -16,19 +19,52 @@ class Screeningservice: CallScreeningService() {
         response: CallResponse.Builder,
         phoneNumber: String
     ): CallResponse.Builder {
+        println(String.format("Incoming call from %s", phoneNumber))
 
-        val FORBIDDEN_PHONE_CALL_NUMBER = "<<PHONE NUMBER>>"
-        if (FORBIDDEN_PHONE_CALL_NUMBER in phoneNumber) {
-            response.apply {
-                setRejectCall(true)
-                setDisallowCall(true)
-                setSkipCallLog(true)
-                //
-                println((String.format("Rejected call from %s", phoneNumber)))
+        val localStorage = CallBlacklistStorage(this.applicationContext)
+        val array = localStorage.getAll()
+        val countryBlockArray = arrayListOf<String>()
+        val numberBlockArray = arrayListOf<String>()
+        val regexBlockArray = arrayListOf<String>()
+
+        for(i in array){
+            if(i.by_country.isNotEmpty()) {
+                countryBlockArray.add(i.by_country)
             }
-        } else {
-            println(String.format("Incoming call from %s", phoneNumber))
+            if(i.by_number.isNotEmpty()) {
+                numberBlockArray.add(i.by_number)
+            }
+            if(i.by_regex.isNotEmpty()) {
+                regexBlockArray.add(i.by_regex)
+            }
         }
+
+        if(countryBlockArray.size > 0){
+            val cleanArray = cleanCountry(countryBlockArray)
+
+            for (i in cleanArray){
+                if(i in phoneNumber){
+                    blockNumber(response,phoneNumber)
+                }
+            }
+        }
+        if(numberBlockArray.size > 0){
+            val cleanArray = cleanNumber(numberBlockArray)
+            for(i in cleanArray){
+                if(i in phoneNumber){
+                    blockNumber(response,phoneNumber)
+                }
+            }
+        }
+        if(regexBlockArray.size > 0){
+            val cleanArray = cleanRegex(regexBlockArray)
+            for(i in cleanArray){
+                if(phoneNumber.matches(i.toRegex())){
+                    blockNumber(response,phoneNumber)
+                }
+            }
+        }
+
         return response
     }
 
@@ -36,4 +72,48 @@ class Screeningservice: CallScreeningService() {
         return callDetails.handle.toString().replace("tel:","").replace("%2B","+")
         //.removeTelPrefix().parseCountryCode()
     }
+
+    private fun cleanCountry(countryArray: ArrayList<String>): java.util.ArrayList<String>{
+        val cleanArray = arrayListOf<String>()
+
+        for (i in countryArray){
+            cleanArray.add(i.split("(")[1].replace(")","").replace("(","").trim())
+        }
+
+        return cleanArray
+    }
+
+    private fun cleanNumber(numberArray: ArrayList<String>): java.util.ArrayList<String>{
+        val cleanArray = arrayListOf<String>()
+
+        for (i in numberArray){
+
+            //If the first number is 0, drop the 0
+            if(i.get(0).toString().equals("0")){
+                cleanArray.add(i.drop(1).trim())
+            }else{
+                cleanArray.add(i.trim())
+            }
+        }
+        return cleanArray
+    }
+
+    private fun cleanRegex(regexArray: ArrayList<String>): java.util.ArrayList<String>{
+        val cleanArray = arrayListOf<String>()
+
+        for(i in regexArray){
+            cleanArray.add(i.trim())
+        }
+        return cleanArray
+    }
+
+    private fun blockNumber(response: Builder,phoneNumber: String){
+        response.apply {
+            println((String.format("Rejected call from %s", phoneNumber)))
+            setRejectCall(true)
+            setDisallowCall(true)
+            setSkipCallLog(true)
+        }
+    }
+
 }
